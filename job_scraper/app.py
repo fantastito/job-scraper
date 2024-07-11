7f5c3d3967bc07b0dacf63a3453463a1fd9bcac1
Simon Budden <simon.budden@gmail.com>
2024-07-11 15:00:25 +0100

    Link flattening added

--- job_scraper/app.py
+++ job_scraper/app.py
@@ -5,11 +5,14 @@ def lambda_handler(event, context):
     
     scraping_results = []
     
+    # Append the result of scrapers to scraping_results
     scraping_results.append(yoto_scraper())
-    # print(email_body)
-
-    #Convert list of strings into single string
-    email_body = '\n'.join(scraping_results)
+    
+    # List compresension to flatten format into string
+    flattened_results = [item for sublist in scraping_results for item in sublist]
+    
+    # Convert list of strings into a single string with HTML formatting
+    email_body = '<br>'.join(flattened_results)
 
     send_email(email_body)
     return {"statusCode": 200}
